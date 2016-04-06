8a438d6fa98e1bdbc794578c1cc02e99c66075b3
Jbernabe <jaime2988bernabe@gmail.com>
2016-04-06 13:25:45 +0200

    ex01 finished with a litte bug

--- D00/ex01/main.cpp
+++ D00/ex01/main.cpp
@@ -13,14 +13,15 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <iomanip>      // std::setw
 #include "Contact.class.hpp"
 
 
-int addContact(Contact currentContact)
+int addContact(Contact *currentContact)
 {
   int const num_options(11);
   std::string options[num_options];
-
+//set and get line funcion to detect the empty lines
   options[0] = ("Please add your Conctact first name: ");
   options[1] = ("Please add your Conctact last name: ");
   options[2] = ("Please add your Conctact nickname: ");
@@ -33,33 +34,84 @@ int addContact(Contact currentContact)
   options[9] = ("Please add your Conctact underwear color: ");
   options[10] = ("Please add your Conctact darkest secret: ");
   std::cout << options[0] << std::endl;
-  getline(std::cin, currentContact.firstName);
+  getline(std::cin, currentContact->firstName);
   std::cout << options[1] << std::endl;
-  getline(std::cin, currentContact.lastName);
+  getline(std::cin, currentContact->lastName);
   std::cout << options[2] << std::endl;
-  getline(std::cin, currentContact.nickName);
+  getline(std::cin, currentContact->nickName);
   std::cout << options[3] << std::endl;
-  getline(std::cin, currentContact.login);
+  getline(std::cin, currentContact->login);
   std::cout << options[4] << std::endl;
-  getline(std::cin, currentContact.postal_address);
+  getline(std::cin, currentContact->postal_address);
   std::cout << options[5] << std::endl;
-  getline(std::cin, currentContact.email_address);
+  getline(std::cin, currentContact->email_address);
   std::cout << options[6] << std::endl;
-  getline(std::cin, currentContact.phone_numer);
+  getline(std::cin, currentContact->phone_numer);
   std::cout << options[7] << std::endl;
-  getline(std::cin, currentContact.birthday_date);
+  getline(std::cin, currentContact->birthday_date);
   std::cout << options[8] << std::endl;
-  getline(std::cin, currentContact.favorite_meal);
+  getline(std::cin, currentContact->favorite_meal);
   std::cout << options[9] << std::endl;
-  getline(std::cin, currentContact.underwear_color);
+  getline(std::cin, currentContact->underwear_color);
   std::cout << options[10] << std::endl;
-  getline(std::cin, currentContact.darkest_secret);
+  getline(std::cin, currentContact->darkest_secret);
   return 1;
 }
 
 void  searchCommand(Contact *allContact)
 {
-  std::cout << allContact->numObjs() << std::endl;
+  std::string fname,lname, nname, id_choise;
+  std::string::size_type sz;   // alias of size_t
+  int id_c;
+
+  for (size_t i = 0;!allContact[i].firstName.empty(); i++)
+  {
+    fname = allContact[i].firstName;
+    lname = allContact[i].lastName;
+    nname = allContact[i].nickName;
+
+    std::cout << std::setiosflags (std::ios::internal | std::ios::right);
+    std::cout << std::setw(10) << i;
+    std::cout << "|";
+    std::cout << std::setiosflags (std::ios::internal | std::ios::right);
+    fname.resize (10,'.');
+    fname.replace(10,1,1,'.');
+    std::cout << std::setw(10) << fname;
+    std::cout << "|";
+    std::cout << std::setiosflags (std::ios::internal | std::ios::right);
+    lname.resize (10,'.');
+    lname.replace(10,1,1,'.');
+    std::cout << std::setw(10) << lname;
+    std::cout << "|";
+    std::cout << std::setiosflags (std::ios::internal | std::ios::right);
+    nname.resize (10,'.');
+    nname.replace(10,1,1,'.');
+    std::cout << std::setw(10) << nname;
+    std::cout << std::endl;
+    std::cout << "Index" << std::endl;
+    while (getline(std::cin, id_choise))
+    {
+      id_c =  std::stoi(id_choise, &sz);
+      if (id_c <= 9 && !allContact[id_c].firstName.empty())
+      {
+          std::cout << allContact[id_c].firstName << std::endl;
+          std::cout << allContact[id_c].lastName << std::endl;
+          std::cout << allContact[id_c].nickName << std::endl;
+          std::cout << allContact[id_c].postal_address << std::endl;
+          std::cout << allContact[id_c].postal_address << std::endl;
+          std::cout << allContact[id_c].email_address << std::endl;
+          std::cout << allContact[id_c].phone_numer << std::endl;
+          std::cout << allContact[id_c].birthday_date << std::endl;
+          std::cout << allContact[id_c].favorite_meal << std::endl;
+          std::cout << allContact[id_c].underwear_color << std::endl;
+          std::cout << allContact[id_c].darkest_secret << std::endl;
+          return ;
+      }
+      else{
+        std::cout << "Choose a good index" << std::endl;
+      }
+    }
+  }
 }
 
 int main()
@@ -75,7 +127,6 @@ int main()
   for (i = 0; i < 10; i++)
   {
     objContact[i].setIndex(i);
-    optContact[i].firstName = "NULL";
   }
 
   i = 0;
@@ -88,8 +139,11 @@ int main()
         searchCommand(objContact);
       else if (str.compare(add_var) == 0)
       {
-        if (addContact(objContact[i]) == 1 && i != 9)
+        if (addContact(&objContact[i]) == 1 && i != 9)
+        {
+          str.clear();
           i++;
+        }
         else
           std::cout << "Your have complete your list - Please user: SEARCH OR EXIT" << std::endl;
       }
